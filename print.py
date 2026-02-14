66e5bc4a73836b363b6e996875ac3ebb10bb6cdb
ajay <er.ajycse@gmail.com>
2026-02-14 22:34:41 +0530

    first .py file and print()function practice

---
+++ print.py
@@ -0,0 +1,43 @@
+# print("welcome")
+# x=5 
+# print(x)
+# y=10
+# print(y)
+# print(x,y,20)#extra constant value print
+# print(x+y+1)#expression+1
+
+
+#formated texgt containing variable(two method below)
+# a=3
+# b=4
+# print("sum of %d and %d is %d"%(a,b,a+b))
+# print("sum of",a,"and",b,"is",a+b)
+
+
+# a=10
+# b=20
+# c=30
+#default putput saperator is ,
+# print(a,b,c)
+                            # #using saperator
+# print(a,b,c,sep="_")
+# print(a,b,c,sep="_",end="\t")
+
+# print(a)
+# print(b)
+# print(c)
+
+
+                        #using end
+# print(a,end=" ")
+# print(b,end="\n\n\n")
+# print(c)
+
+
+
+# Escape sequences    
+# print("hello\rA")#will replace first latter of hello and become aello
+# print("\\")
+# print("\"")
+# print("\'")
+# print("hello\b")
