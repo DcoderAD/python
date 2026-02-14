1e2516d4813cfff2d967d38a0c5191db1d8fccef
ajay <er.ajycse@gmail.com>
2026-02-14 23:20:00 +0530

    learn keyword and help() function in python on python shell

---
+++ keyword_&_help.py
@@ -0,0 +1,16 @@
+# import keyword      #importing a module name keyword
+# print(keyword.kwlist)
+# print(keyword.softkwlist)
+
+
+# from keyword import kwlist  #this is the another method to import 
+# print(kwlist)
+
+# 'False', 'None', 'True'---->all three has fist font capital all three has speciallity all three are used as data too  
+# x=False
+# y=None
+# z=True
+
+
+# '_', 'case', 'match', 'type'----->
+# these 4 are softkeywords 
