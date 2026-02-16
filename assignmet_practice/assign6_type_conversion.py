dbab565142aadfa4b27bda3ae7aaa00d4d142709
ajay <er.ajycse@gmail.com>
2026-02-16 20:50:15 +0530

    learn type conversion in this file

---
+++ assignmet_practice/assign6_type_conversion.py
@@ -0,0 +1,26 @@
+#convert num into str
+# a=10
+# print(type(str(a)))
+
+# unicode of m
+# a='m'
+# print(ord(a))  #ord()function to print unicode
+
+# char representation of 100
+# a=100
+# print(chr(a))  #use chr function to get char representation of unicode
+
+
+#convert str in int and describe when it is not possible
+# a='100'  #we can convert it into int 
+# b='A'   #this cant be converted into int
+# print(type(a))
+# print(type(int(a)))
+
+
+
+#convert int into bool
+# a=10     #any non zero will be true
+# b=0
+# print(bool(a))
+# print(bool(b))
