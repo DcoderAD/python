a5d4870821134e4a46d6fa8de200b44afa6fd9cc
ajay <er.ajycse@gmail.com>
2026-02-16 22:54:01 +0530

    user input practice

---
+++ assignmet_practice/assign8_user_input.py
@@ -0,0 +1,19 @@
+#take your name as input and print it
+# a=print(type(input("enter your name"))) 
+#bydefault input fun.str hi return krta hai so we dont need type conversion here
+
+# #take two num from user and add them and print it
+# print(int(input("Enter first number"))+int(input("Enter second num")))
+
+
+#take radius from user and calculate the area of circle
+# r=float(input("Enter the radius of circle"))
+# print("area of circle is",2.14*r*r)
+
+#take a number from user and so squre of it
+# a=int(input("Enter a number"))
+# print(a*a)
+
+#take char from user and print its unicode
+# a=input("Enter a character")
+# print(ord(a)) #use ord to see the unicode of enterd char
