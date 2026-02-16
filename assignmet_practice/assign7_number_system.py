aba95c74188e935a5ad299b5e6a68854ab598d02
ajay <er.ajycse@gmail.com>
2026-02-16 22:53:22 +0530

    number system practice

---
+++ assignmet_practice/assign7_number_system.py
@@ -0,0 +1,21 @@
+#any num and its binary
+# a=10
+# print(bin(a)) #bin() function convert num into binary
+
+
+# store a binary num 1100101 in a var and print it in decimal
+# a=0b1100101
+# print(a)   #it will bydefault convert into decimal
+
+#store hexadecimal num 2f in var and print it in octal
+# a=0x2f
+# print(type(oct(a)))
+
+# store octal num 125 in a variable print binary of it
+# a=0o125
+# print(bin(a)) 
+
+# add 25 in octal and 39 in hexadecimal and print result in binary
+# a=0o25
+# b=0x39
+# print(bin(a+b))
