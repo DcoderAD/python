41d668c571446b512186860c0b0d942a930768d7
ajay <er.ajycse@gmail.com>
2026-02-16 20:34:41 +0530

    modified about 3 keyword use as data

---
+++ assignmet_practice/assign5_import_&_keyward.py
@@ -0,0 +1,13 @@
+#print all keyward on screen
+# 1st method
+# import keyword
+# print(keyword.kwlist,sep='\n') #why \n is not working with the list
+
+# 2nd method
+# from keyword import kwlist
+# print(kwlist,sep='\n')#here also \n is not working why??
+
+#3rd method go into python shell and print keyword using help() function
+
+#there are 3 keyword which used as data too.only these 3 keywords starts with capital 
+          
