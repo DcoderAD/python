3b31bd168d45840699c9b8e26617c08cbdda3a68
ajay <er.ajycse@gmail.com>
2026-02-16 19:43:48 +0530

    playing with variable

---
+++ assignmet_practice/assign4_variable.py
@@ -0,0 +1,35 @@
+#print value of a
+# a= 12
+# print(a)
+
+
+#print your name
+# a='ajay'
+# print(a)
+
+
+#print 3 var in new line
+# a=22
+# b=22
+# c=33
+# print(a,b,c,sep='\n')
+
+
+# print 5 diff var with their datatype 
+# a=1
+# b='A'
+# c=True
+# d=4.5
+# e=4+6j
+# print(a,type(a),sep='-->')
+# print(b,type(b),sep='-->')
+# print(c,type(c),sep='-->')
+# print(d,type(d),sep='-->')
+# print(e,type(e),sep='-->')
+
+
+#3var containig current day month and year and print in proper format like d/m/y
+# d=16
+# m=2
+# y=2026
+# print(d,m,y,sep='/')
