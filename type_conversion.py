944e3a1e704a890fcaa0fe8dce546776f6f4106f
ajay <er.ajycse@gmail.com>
2026-02-16 19:27:17 +0530

    type conversion in python

---
+++ type_conversion.py
@@ -0,0 +1,25 @@
+# x=23
+# y=0b10111
+# print(bin(x))
+# print(hex(x))
+# print(oct(x))
+
+# #without using the conversion function it will just give the output in decimal 
+# x=0o43
+# y=0b10111
+# z=0x34
+# n=0x2f
+# print(x)
+# print(y)
+# print(z)
+# print(n)
+
+
+# kisi single digit str ka unicode kese pata kare??
+# a="A"
+# print(a,ord(a)) #this ord function will give you the unicode of alphabate
+
+
+#unicode diya hua hai to char kese pata kare
+b=100
+print(b,chr(b))#chr function given unicode ko uske corrospponding chr character me conver kar dega
