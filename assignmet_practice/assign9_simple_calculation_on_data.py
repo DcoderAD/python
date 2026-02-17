2bd25167d9edad117687e2533de4f5ea62aef531
ajay <er.ajycse@gmail.com>
2026-02-17 12:09:38 +0530

    calculation on some of data

---
+++ assignmet_practice/assign9_simple_calculation_on_data.py
@@ -0,0 +1,32 @@
+#simple interest
+# p=float(input("Enter the principle amount"))
+# i=float(input("Enter the interest rate"))
+# t=float(input("Enter the time"))
+# si=(p*i*t)/100
+# print(si)
+
+
+#area of a rectangle
+# l=int(input("Enter the length of rectangle"))
+# w=int(input("Enter the width"))
+# area_of_rectangle=l*w
+# print("area of rectangle is ",area_of_rectangle)
+
+
+#average of three number
+# a=int(input("Enter first number \n"))
+# b=int(input("Enter second number \n"))
+# c=int(input("Enter third number \n"))
+# print("average of a,b,c is ",(a+b+c)/3)
+
+#volume of cuboid
+# l=int(input("enter the length of cuboid"))
+# w=int(input("enter the width of cuboid"))
+# h=int(input("enter the height of cuboid"))
+# volume=l*w*h
+# print("volume of cuboid is ",volume)
+
+# multi of two num.
+# x=int(input("enter x"))
+# y=int(input("enter y"))
+# print(x*y)
