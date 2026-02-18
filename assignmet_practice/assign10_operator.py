5c32dd548dc3fe85c3b348620a8f429cf81b4004
ajay <er.ajycse@gmail.com>
2026-02-18 13:22:38 +0530

    operator assignment

---
+++ assignmet_practice/assign10_operator.py
@@ -0,0 +1,26 @@
+#remove the last digit from number 2345
+# a=int(input("enter a number\n")) #enter 2345
+# print(a//10)
+
+#get the last digit from the entered number
+# a=int(input("Enter a digit"))
+# print(a%10)
+
+#swap data of two variable
+# a=int(input("Enter a number"))
+# b=int(input("Enter a number"))
+# print(a,b)
+# c=a
+# a=b
+# b=c
+# print(a,b)
+
+#take 3 digit num from user and only show first number only
+# a=int(input("Enter a 3 digit number"))
+# a=(a//10)//10
+# print(a)
+
+#take 3 digit number and show only middle digit in output
+# a=int(input("Enter a three digit number"))
+# a=(a//10)%10
+# print(a)
