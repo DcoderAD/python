2420fa58a53eb3b45443404088096d86809962f9
ajay <er.ajycse@gmail.com>
2026-02-19 20:49:07 +0530

    assignment operator

---
+++ assignmet_practice/assignment11_operator.py
@@ -0,0 +1,33 @@
+#print true if the 'my' is the part of entered string by user
+# s="hey this is my bike"
+# print('my' in s)
+# print("my" not in s)
+
+# input two string from usercheck if 2 variable refered to the same object
+a='hello'
+b='hello'
+print(a is b)
+
+
+
+a1=input("Enter first string\n")
+a2=input("Enter second string\n")
+print(a1 is a2)
+
+a1 and a2 me input function use krke same string input dene pe iska answere False kyu aa raha hai??? 
+a1 and a2 same object ko refer nahi kar rahe hai??
+
+# a=10
+# b=10
+# print(a is b)
+
+# result of an expression
+# a=5>10<5
+# print(a)
+ 
+# a="red" and "white"
+# print(a)
+
+# a= True or False
+# print(a)
+
